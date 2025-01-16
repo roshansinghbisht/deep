e2ecec7efe671f798d4c6c7ddd14619e23643ea3
Roshan Bisht <roshanbisht@Roshans-MacBook-Air-M2.local>
2025-01-16 08:15:08 +0530

    first

---
+++ home/model.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+    
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+
+    
+    def forward(self, features, captions):
+        embeddings = self.embed(captions[:,:-1])
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _ = self.lstm(embeddings)
+        outputs = self.fc(hiddens)
+        return outputs
+          
+
+
+    def sample(self, inputs, states=None, max_len=20):
+        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        sampled_ids = []
+        for i in range(20):                                   
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.fc(hiddens.squeeze(1))
+            max_index = outputs.max(1)[1]
+            sampled_ids.append(max_index.item())
+
+            inputs = self.embed(max_index)
+            inputs = inputs.unsqueeze(1) 
+        return sampled_ids
+            
+        
+        
